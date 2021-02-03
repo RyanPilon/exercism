ee995578b72e81b20df62c05340704a93483567d
Ryan Pilon <ryanpilon1995@gmail.com>
2021-02-03 15:39:07 -0800

    added javascript track

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,7 +1,4 @@
-def two_fer(name=None):
-    if name is None:
-        return "One for you, one for me."
-    else:
-        return f"One for { name }, one for me."
+def two_fer(name='you'):
+    return f"One for { name }, one for me."
 
     
